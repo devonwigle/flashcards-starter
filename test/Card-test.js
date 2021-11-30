18d55e088cd542d82dc41bf52e875ee7f65d2cd2
Devon Wigle <devonwigle@gmail.com>
2021-11-29 20:09:00 -0600

    Pass instance of card and should have id tests

--- test/Card-test.js
+++ test/Card-test.js
@@ -10,12 +10,12 @@ describe('Card', function() {
     expect(Card).to.be.a('function');
   });
 
-  it.skip('should be an instance of Card', function() {
+  it('should be an instance of Card', function() {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
   }); 
 
-  it.skip('should have an id', function() {
+  it('should have an id', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.id).to.equal(1);
   });
