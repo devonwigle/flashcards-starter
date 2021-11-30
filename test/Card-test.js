5bc07bd3ab2ce3cbcc1fafe538f6befe562f1808
Devon Wigle <devonwigle@gmail.com>
2021-11-29 20:32:23 -0600

    Pass store a list of possible answers test

--- test/Card-test.js
+++ test/Card-test.js
@@ -25,9 +25,10 @@ describe('Card', function() {
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
-  it.skip('should store a list of possible answers', function() {
+  it('should store a list of possible answers', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
+
   });  
 
   it.skip('should store the correct answer', function() {
