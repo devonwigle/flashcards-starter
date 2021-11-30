7b508dc6e32a14ca40f5a3cb3f01e6ea1398966e
Devon Wigle <devonwigle@gmail.com>
2021-11-29 20:33:03 -0600

    Pass store correct answer test

--- test/Card-test.js
+++ test/Card-test.js
@@ -31,7 +31,7 @@ describe('Card', function() {
 
   });  
 
-  it.skip('should store the correct answer', function() {
+  it('should store the correct answer', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
   });
