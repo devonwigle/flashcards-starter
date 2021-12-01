570e0d628b65d8251f61c58ea37d661780f65dad
Devon Wigle <devonwigle@gmail.com>
2021-12-01 16:34:10 -0600

    Replace array with smallData set file

--- test/Round-test.js
+++ test/Round-test.js
@@ -3,28 +3,13 @@ const expect = chai.expect;
 
 const Round = require('../src/Round');
 const Turn = require('../src/Turn');
-const Deck = require('../src/Deck');
-const smallDeck = [{
-  id: 1,
-  question: 'What allows you to define a set of related information using key-value pairs?',
-  answers: ['object', 'array', 'function'],
-  correctAnswer: 'object'
-}, {
-  id: 2,
-  question: 'What is a comma-separated list of related values?',
-  answers: ['array', 'object', 'function'],
-  correctAnswer: 'array'
-}, {
-  id: 3,
-  question: 'What type of prototype method directly modifies the existing array?',
-  answers: ['mutator method', 'accessor method', 'iteration method'],
-  correctAnswer: 'mutator method'
-}];
+const Deck = require('../src/smallData');
 
 describe('Round', function() {
-  it('should return the current card being played', function() {
-    const round = new Round()
+  it.only('should return the current card being played', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
+    const deck = new Deck(smallDeck)
+    const round = new Round(deck)
     const turn = new Turn('object', card);
 
     round.returnCurrentCard();
