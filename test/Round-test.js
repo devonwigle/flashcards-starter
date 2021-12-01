fefaf0e20e7ebcd126a1da22888ea2bea88cac61
Devon Wigle <devonwigle@gmail.com>
2021-12-01 05:27:34 -0600

    Write test for return card being played

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,3 +2,15 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Round = require('../src/Round');
+const Turn = require('../src/Turn');
+
+describe('Round', function() {
+  it('should return the current card being played', function() {
+    const round = new Round()
+    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    round.returnCurrentCard();
+    expect(round.returnCurrentCard()).to.deep.equal({ id: 1, question: 'What allows you to define a set of related information using key - value pairs ? ', answers: ['object', 'array', 'function'], correctAnswer: 'object' })
+  })
+})
