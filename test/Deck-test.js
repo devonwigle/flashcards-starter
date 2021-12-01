a627a037a9619545d7ff4b25e347cfd867b45934
Devon Wigle <devonwigle@gmail.com>
2021-11-30 21:07:16 -0600

    Add array and length tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,3 +3,15 @@ const expect = chai.expect;
 
 const Deck = require('../src/Deck')
 const data = require('../src/data')
+
+describe('Deck', function() {
+  it('should contain an array', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an(array);
+  })
+
+  it('should contain 30 objects', function() {
+    const deck = new Deck();
+    expect(deck).to.have.lengthOf(30);
+  })
+})
