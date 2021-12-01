bc26aa95bbe81b5f1f313dece3f5c9398cb1a9b5
Devon Wigle <devonwigle@gmail.com>
2021-11-30 21:33:49 -0600

    Fix method invocation and add quotes to array

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -7,11 +7,11 @@ const data = require('../src/data')
 describe('Deck', function() {
   it('should contain an array', function() {
     const deck = new Deck(data.prototypeData);
-    expect(deck.pack).to.be.an(array);
+    expect(deck.pack).to.be.an('array');
   })
 
   it('should contain 30 objects', function() {
     const deck = new Deck();
-    expect(deck.countCards).to.equal(30);
+    expect(deck.countCards()).to.equal(30);
   })
 })
