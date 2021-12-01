972f835f1096e0d21b3ed90c4ebebb534e7477e6
Devon Wigle <devonwigle@gmail.com>
2021-12-01 17:13:24 -0600

    Make properties for turns and incorrectGuesses, fix path for returnCurrentCard method

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,12 @@
 class Round {
-
+  constructor(deck) {
+    this.deck = deck.pack;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+  returnCurrentCard() {
+    return this.deck.pack[this.turns];
+  }
 }
 
 module.exports = Round;
