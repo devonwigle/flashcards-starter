5500b2a750387d5961a82595ad7cc4e6e1139153
Devon Wigle <devonwigle@gmail.com>
2021-12-01 05:38:33 -0600

    Write new instance of turn test

--- test/Round-test.js
+++ test/Round-test.js
@@ -11,6 +11,17 @@ describe('Round', function() {
     const turn = new Turn('object', card);
 
     round.returnCurrentCard();
+    
     expect(round.returnCurrentCard()).to.deep.equal({ id: 1, question: 'What allows you to define a set of related information using key - value pairs ? ', answers: ['object', 'array', 'function'], correctAnswer: 'object' })
+  })
+
+  it('should create a new instance of Turn', function() {
+    const round = new Round()
+    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    round.takeTurn();
+
+    expect(round.takeTurn()).to.be.an.instanceOf(turn, Turn);
   })
 })
