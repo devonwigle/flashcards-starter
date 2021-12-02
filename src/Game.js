df9cc8bf1319a843516755f59dc64f2ad2d6888f
Devon Wigle <devonwigle@gmail.com>
2021-12-02 15:03:06 -0600

    Fix cards

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,4 @@
+const Card = require('./Card');
 const data = require('./data');
 const Deck = require('./Deck');
 const Round = require('./Round');
@@ -19,9 +20,14 @@ class Game {
   }
 
   start() {
-    const cards = prototypeQuestions;
+    const cards = prototypeQuestions.map(card => {
+      let card = new Card(card.id, card.question, card.answers, card.correctAnswer);
+      return card;
+    }
     const deck = new Deck(cards);
     this.currentRound = new Round(deck)
+    this.printMessage(deck);
+    this.printQuestion(round);
   }
 }
 
