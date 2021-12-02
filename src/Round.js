71076d9cd4d804b855bc281345829fa5c414e3a1
Devon Wigle <devonwigle@gmail.com>
2021-12-01 21:24:20 -0600

    Build more of round class

--- src/Round.js
+++ src/Round.js
@@ -1,14 +1,23 @@
+const Turn = require("./Turn");
+
 class Round {
   constructor(deck) {
     this.deck = deck.pack;
     this.turns = 0;
     this.incorrectGuesses = [];
+    this.turn;
   }
   returnCurrentCard() {
     return this.deck[this.turns];
   }
   takeTurn(guess) {
-
+    const currentCard = this.returnCurrentCard();
+    this.turns++;
+    this.turn = new Turn(guess, currentCard)
+    if (!this.turn.evaluateGuess()) {
+      this.incorrectGuesses.push(currentCard.id)
+    }
+    
   }
 }
 
