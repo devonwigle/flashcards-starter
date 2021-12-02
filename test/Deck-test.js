0e51156a7f516da8cd078b3fcd182dfbe0ba1ca7
Devon Wigle <devonwigle@gmail.com>
2021-12-02 05:18:48 -0600

    Update more arrow functions

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,13 +4,13 @@ const expect = chai.expect;
 const Deck = require('../src/Deck')
 const data = require('../src/data')
 
-describe('Deck', function() {
-  it('should contain an array', function() {
+describe('Deck', () => {
+  it('should contain an array', () => {
     const deck = new Deck(data.prototypeData);
     expect(deck.pack).to.be.an('array');
   })
 
-  it('should contain 30 objects', function() {
+  it('should contain 30 objects', () => {
     const deck = new Deck(data.prototypeData);
     expect(deck.countCards()).to.equal(30);
   })
