740b31a2509a7bd78fa0b9c60ff359f70759eacc
Devon Wigle <devonwigle@gmail.com>
2021-12-02 05:35:53 -0600

    Add beforeEach for card and turn variables

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,24 +5,25 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Turn', () => {
+  let card;
+  let turn;
+
+  beforeEach(() => {
+    card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
+    turn = new Turn('object', card);
+  })
 
   it('should have a user guess', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
 
     expect(turn.userGuess).to.be.a('string');
   });
 
   it('should have a current card', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
 
     expect(turn.card).to.be.an('object');
   });
 
   it('should return user guess', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
 
     turn.returnGuess();
 
@@ -30,8 +31,6 @@ describe('Turn', () => {
   });
 
   it('should return the current card', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
 
     turn.returnCard();
 
@@ -39,8 +38,6 @@ describe('Turn', () => {
   });
 
   it('should evaluate as a boolean', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
 
     turn.evaluateGuess();
 
@@ -48,8 +45,6 @@ describe('Turn', () => {
   });
 
   it('should return whether the guess is correct', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
 
     turn.giveFeedback();
 
@@ -57,8 +52,7 @@ describe('Turn', () => {
   });
 
   it('should return whether the guess is incorrect', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key - value pairs ? ', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('array', card);
+    let turn = new Turn('array', card);
 
     turn.giveFeedback();
 
