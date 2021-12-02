be6e827974cd2475a69826afe485e9c4f2a0d17c
Devon Wigle <devonwigle@gmail.com>
2021-12-02 05:22:18 -0600

    Delint semicolon and indentation

--- src/Round.js
+++ src/Round.js
@@ -7,20 +7,23 @@ class Round {
     this.incorrectGuesses = [];
     this.turn;
   }
+
   returnCurrentCard() {
     return this.deck[this.turns];
   }
+
   takeTurn(guess) {
     const currentCard = this.returnCurrentCard();
     this.turns++;
     this.turn = new Turn(guess, currentCard)
     if (!this.turn.evaluateGuess()) {
       this.incorrectGuesses.push(currentCard.id)
-    }
+  }
     this.turn.giveFeedback()
-    };
+    }
+
   calculatePercentCorrect() {
-    const percent = Math.round(10 * (1 - (this.incorrectGuesses.length / this.deck.length)) * 100) / 10
+    let percent = Math.round(10 * (1 - (this.incorrectGuesses.length / this.deck.length)) * 100) / 10
     return percent
   }
   endRound() {
